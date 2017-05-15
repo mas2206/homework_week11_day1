c90acf6b9200cf912414c5c9a217c56d4fe7bc8e
Michael Shennan <m.shennan22@gmail.com>
2017-05-15 20:39:13 +0100

    changed image links to full path instead of going through link shortener

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 var catsArray = [
-  {name: "Boba", favFood: "Sock fluff", image: "http://bit.ly/2riTB4W"},
-  {name: "Barnaby", favFood: "Tuna", image: "http://bit.ly/2aI3Nw4"},
-  {name: "Max", favFood: "Whiskas Temptations", image: "http://bit.ly/2qkvGoE"},
-  {name: "Rascal", favFood: "Ham", image: "http://bit.ly/2pDsCF4"}
+  {name: "Boba", favFood: "Sock fluff", image: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+  {name: "Barnaby", favFood: "Tuna", image: "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+  {name: "Max", favFood: "Whiskas Temptations", image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"},
+  {name: "Rascal", favFood: "Ham", image: "http://img.huffingtonpost.com/asset/scalefit_720_noupscale/582cb2601a00002400cc8426.jpeg?cache=wltod2kvw5"}
 ];
 
 var createCatList = function() {
