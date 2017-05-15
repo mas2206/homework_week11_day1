d7151506cc80ef7d9d88b335dcbdea41103da540
Michael Shennan <m.shennan22@gmail.com>
2017-05-15 20:48:45 +0100

    added garfield

--- public/app.js
+++ public/app.js
@@ -2,7 +2,8 @@ var catsArray = [
   {name: "Boba", favFood: "Sock fluff", image: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
   {name: "Barnaby", favFood: "Tuna", image: "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
   {name: "Max", favFood: "Whiskas Temptations", image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"},
-  {name: "Rascal", favFood: "Ham", image: "http://img.huffingtonpost.com/asset/scalefit_720_noupscale/582cb2601a00002400cc8426.jpeg?cache=wltod2kvw5"}
+  {name: "Rascal", favFood: "Ham", image: "http://img.huffingtonpost.com/asset/scalefit_720_noupscale/582cb2601a00002400cc8426.jpeg?cache=wltod2kvw5"},
+  {name: "Garfield", favFood: "Lasagne", image: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bc/Garfield_the_Cat.svg/1200px-Garfield_the_Cat.svg.png"}
 ];
 
 var createCatList = function() {
